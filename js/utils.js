c754c6a888e20c802607ad46367a42c247bd56d5
Andrzej Mazur <andrzej.mazur@end3r.com>
2011-10-18 12:08:08 +0200

    fixed lack of .bind implementation for Opera, Safari and some IEs

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,29 @@
+/*
+	fix for lacking of .bind in Opera, Safari and some IEs
+	https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+*/
+if (!Function.prototype.bind) {  
+	Function.prototype.bind = function (oThis) {  
+	if (typeof this !== "function") {  
+		// closest thing possible to the ECMAScript 5 internal IsCallable function  
+		throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");  
+	}  
+	var fSlice = Array.prototype.slice,  
+		aArgs = fSlice.call(arguments, 1),   
+		fToBind = this,   
+		fNOP = function () {},  
+		fBound = function () {  
+		return fToBind.apply(this instanceof fNOP  
+			? this  
+			: oThis || window,  
+			aArgs.concat(fSlice.call(arguments)));  
+		};
+		fNOP.prototype = this.prototype;  
+		fBound.prototype = new fNOP();  
+		return fBound;  
+	};  
+};
+
 GAME.Utils = {};
 GAME.$id = function(id) { return document.getElementById(id); };
 
